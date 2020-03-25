8408403b6f43ebe4bf6030e77f2fe84ea63ec067
Krystian Sciuba <krystiansciuba@gmail.com>
2020-03-25 22:32:02 +0100

    some simple changes

--- src/try_django/views.py
+++ src/try_django/views.py
@@ -1,6 +1,4 @@
-from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from django.template.loader import get_template
 from blog.models import BlogPost
 from .forms import UserForm
 from django.db import connection
@@ -8,26 +6,30 @@ from django.contrib.auth import authenticate, login, logout
 
 
 def home_page(request):
-	my_title="Hello there!!!"
-	return render(request, "home.html", {"title": my_title})
+	template="home.html"
+	return render(request, template)
 
 def about_page(request):
-	return render(request, "about.html",{"title":"About Us"})
+	template="about.html"
+	return render(request, template)
 
 def login_page(request):
+	template="login.html"
 	if request.method=='POST':
 		username = request.POST['username']
 		password = request.POST['password']
 		user = authenticate(username=username, password=password)
 		if user is not None:
 			login(request, user)
-			return redirect ('home_page')
+			return redirect ('login')
 		else:
-			return render(request, "login.html", {"message": "invalid credentials"})
+			message={"message": "invalid credentials"}
+			return render(request, template, message)
 	else:
-		return render(request, "login.html", {"title": "LOG IN"})
+		return render(request, template)
 
 def register_page(request):
+	template='register.html'
 	new_user_form = UserForm(request.POST or None)
 
 	if new_user_form.is_valid():
@@ -36,41 +38,29 @@ def register_page(request):
 		password=new_user_form.cleaned_data['password']
 		new_user.set_password(password)
 		new_user.save()
-
-		user=authenticate(username=username, password=password)
-		if user is not None:
-			if user.is_authenticated:
-				login(request, user)
-				return redirect ('home_page')
-	template='register.html'
-	return render(request, template, {'new_user_form': new_user_form})
+		if new_user is not None:
+			return redirect ('login')
+	return render(request, template,{'new_user_form': new_user_form})
 
 
 def logout_page(request):
 	logout(request)
-	return redirect ('home_page')
+	return redirect ('login')
 
 def contact_page(request):
-	return render(request, "contact.html",{"title":"Contact Us"})
-
-
-def example_page(request):
-	context			= {"title": "Example"}
-	template_name	= "hello_world.html"
-	template_obj 	= get_template (template_name)
-	return HttpResponse(template_obj.render(context))
+	template='contact.html'
+	return render(request, template)
 
 def test_page(request):
-	my_title="TEST!!!"
-	context={"title":my_title}
+	template='test.html'
 	if request.user.is_authenticated:
-		context={"title":my_title, "my_list": [1, 2, 3, 4, 5]}
-	return render(request, "test.html", context)
+		content={"my_list": [1, 2, 3, 4, 5]}
+		return render(request, template, content)
+	return render(request, template)
 
 def newest_post_page(request):
 	with connection.cursor() as cursor:
 		cursor.execute("SELECT slug FROM blog_blogpost ORDER BY pub_date DESC LIMIT 1")
 		last_post=cursor.fetchone()
 		last_post_slug=last_post[0]
-
 		return redirect('blog_post_detail_view', slug=last_post_slug)
